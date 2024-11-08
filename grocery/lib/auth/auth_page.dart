82cc9da4398dc0a9a5dd81159cb9f1994f58d666
gabrieldiaslm <xgabrieldias777@gmail.com>
2024-11-08 10:41:32 -0300

    prosseguindo com o plano

--- grocery/lib/auth/auth_page.dart
+++ grocery/lib/auth/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:grocery/auth/icon_text_form_field.dart';
+
+import './icon_text_form_field.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -9,36 +10,46 @@ class AuthPage extends StatelessWidget {
     final theme = Theme.of(context);
 
     return Scaffold(
-      body: Column(children: [
-        Expanded(
-          child: Container(
-            color: theme.primaryColor,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical:40.0,
-              horizontal: 32.0
-            ),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-              color: Colors.white
-            ),
-            child: const Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 15.0),
-                  child: IconTextFormField(labelText: 'E-mail',icon: Icons.email,),
-                ),
-                IconTextFormField(isSecret: true, labelText: 'Senha', icon: Icons.lock,),
-              ],
-            ),
-          ),
-        ),
-      ],),
       backgroundColor: theme.primaryColor,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: theme.primaryColor,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32.0,
+                vertical: 40.0,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(40.0),
+                ),
+              ),
+              child: const Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
+                    child: IconTextFormField(
+                      labelText: 'E-mail',
+                      icon: Icons.email,
+                    ),
+                  ),
+                  IconTextFormField(
+                    labelText: 'Senha',
+                    icon: Icons.lock,
+                    isSecret: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
